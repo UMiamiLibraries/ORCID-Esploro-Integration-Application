bbded432133af22b5e767987c4bdf4be9b368ee5
Charles <cgb37@miami.edu>
2023-03-09 15:12:10 -0500

    commit files without config values

--- Env/Prod/app/__init__.py
+++ Env/Prod/app/__init__.py
@@ -10,21 +10,21 @@ app = Flask(__name__)    # Create an instance of the class for our use
 
 app.config.update(
     DEBUG=True,
-    SECRET_KEY='$4d^URjlhGQzRAfCW%t13Zc5w9Wd##',
+    SECRET_KEY='',
     SAML_PATH=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'saml'),
-    MAIL_SERVER='smtp.umail.miami.edu',
+    MAIL_SERVER='',
     MAIL_PORT=25,
     #MAIL_USE_SSL=True
-    EXL_APIKEY='l8xxb34ba7ed439b4b8096615fd11993e5ec',
+    EXL_APIKEY='',
     ESPLORO_RESEARCHER_API='https://api-na.hosted.exlibrisgroup.com/esploro/v1/researchers/',
     ALMA_USER_API='https://api-na.hosted.exlibrisgroup.com/almaws/v1/users/',
     ESPLORO_TOKEN_API='https://api-na.hosted.exlibrisgroup.com/esploro/v1/researchers/{orcid}/token?',
-    ORCID_OAUTH_CLIENT_ID='APP-TQL0JRATUMCBFL5Q',
-    ORCID_OAUTH_CLIENT_SECRET='9ac640ec-ef34-49c1-a8ee-e7dfbaeb972a',
-    SLACK_WEBHOOK='https://hooks.slack.com/services/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',
-    EMAIL_ADMIN='eprieto502@miami.edu',
-    EMAIL_TECHNICAL='cgb37@miami.edu',
-    EMAIL_SUPPORT='repository.library@miami.edu',
+    ORCID_OAUTH_CLIENT_ID='',
+    ORCID_OAUTH_CLIENT_SECRET='',
+    SLACK_WEBHOOK='',
+    EMAIL_ADMIN='',
+    EMAIL_TECHNICAL='',
+    EMAIL_SUPPORT='',
     ORCID_URL='https://orcid.org',
     ORCID_SANDBOX=False
 )
--- Env/Staging/app/__init__.py
+++ Env/Staging/app/__init__.py
@@ -9,22 +9,22 @@ app = Flask(__name__)    # Create an instance of the class for our use
 
 app.config.update(
     DEBUG=True,
-    SECRET_KEY='wYc9PQRAp*jv9kv2VY17bGJN9S7IvQ',
+    SECRET_KEY='',
     SAML_PATH=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'saml'),
-    MAIL_SERVER='smtp.umail.miami.edu',
+    MAIL_SERVER='',
     MAIL_PORT=25,
     #MAIL_USE_SSL=True
-    EXL_APIKEY='l8xx685c01ea135645bc91db002f0e132b2f',
+    EXL_APIKEY='',
     ESPLORO_RESEARCHER_API='https://api-na.hosted.exlibrisgroup.com/esploro/v1/researchers/',
-    ESPLORO_TOKEN_API='https://api-na.hosted.exlibrisgroup.com/esploro/v1/researchers/{orcid}/token?',
     ALMA_USER_API='https://api-na.hosted.exlibrisgroup.com/almaws/v1/users/',
-    ORCID_OAUTH_CLIENT_ID='APP-L0SEY2X8CZ86A586',
-    ORCID_OAUTH_CLIENT_SECRET='9d2cccc3-6595-49e7-8ebf-1143aa37c133',
-    SLACK_WEBHOOK='https://hooks.slack.com/services/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',
-    EMAIL_ADMIN='eprieto502@miami.edu',
-    EMAIL_TECHNICAL='cgb37@miami.edu',
-    EMAIL_SUPPORT='repository.library@miami.edu',
-    ORCID_URL='https://sandbox.orcid.org',
+    ESPLORO_TOKEN_API='https://api-na.hosted.exlibrisgroup.com/esploro/v1/researchers/{orcid}/token?',
+    ORCID_OAUTH_CLIENT_ID='',
+    ORCID_OAUTH_CLIENT_SECRET='',
+    SLACK_WEBHOOK='',
+    EMAIL_ADMIN='',
+    EMAIL_TECHNICAL='',
+    EMAIL_SUPPORT='',
+    ORCID_URL='https://orcid.org',
     ORCID_SANDBOX=True
 )
 mail = Mail(app)
